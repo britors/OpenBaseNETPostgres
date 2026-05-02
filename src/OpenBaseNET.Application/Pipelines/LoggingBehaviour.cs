5850c0c424e275ae0b37cbf2800b157911e941b6
Rodrigo Brito <rodrigo@w3ti.com.br>
2026-05-02 12:03:00 -0300

    Atualizar Pacotes nuget

--- src/OpenBaseNET.Application/Pipelines/LoggingBehaviour.cs
+++ src/OpenBaseNET.Application/Pipelines/LoggingBehaviour.cs
@@ -1,30 +1,15 @@
 ﻿using MediatR;
-using Microsoft.Extensions.Logging;
-using System.Text.Json;
 
 namespace OpenBaseNET.Application.Pipelines;
 
-public sealed class LoggingBehaviour<TRequest, TResponse>(ILogger<LoggingBehaviour<TRequest, TResponse>> logger)
+public sealed class LoggingBehaviour<TRequest, TResponse>
     : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
     public async Task<TResponse> Handle(TRequest request,
         RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        var requestName = typeof(TRequest).Name;
-        try
-        {
-
-            logger.LogInformation("Chamando o comando {Name} com valores {Value}", requestName, JsonSerializer.Serialize(request));
-            var response = await next();
-            logger.LogInformation("Resposta para  o comando {Name} foi {Value}", requestName, JsonSerializer.Serialize(response) );
-            return response;
-        }
-        catch (Exception exception)
-        {
-            logger.LogError(
-                exception, "Ocorreu um erro ao chamar o comando {Name}: {Message}", requestName, exception.Message); 
-            throw; 
-        }
+        var response = await next(cancellationToken);
+        return response;
     }
 }
